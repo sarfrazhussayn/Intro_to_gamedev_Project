cbfa3263e5ca4bf4204366843ded5e5d00c2240c
sarfrazhussayn <35394114+sarfrazhussayn@users.noreply.github.com>
2020-03-31 17:50:04 +0500

    Menu Nav
    Basic Skeleton for game navigation

---
+++ FoodRescuer/Assets/MenuControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void BackButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit Called");
+    }
+
+}
