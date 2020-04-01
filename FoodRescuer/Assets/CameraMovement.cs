c78df86bbbfa345e5848da5e4cb27e211400e3cf
sarfrazhussayn <35394114+sarfrazhussayn@users.noreply.github.com>
2020-04-01 23:57:05 +0500

    Menu & Tutorial
    Menu navigation complete.
    Tutorial Level Added as well

---
+++ FoodRescuer/Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.5f;
+
+    private void Update()
+    {
+        transform.position = target.position;
+    }
+}
--- FoodRescuer/Assets/MenuControl.cs
+++ FoodRescuer/Assets/MenuControl.cs
@@ -8,14 +8,26 @@ public class MenuControl : MonoBehaviour
     // Start is called before the first frame update
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+        Debug.Log("PlayGame Called");
     }
     
     public void BackButton()
     {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("BackButton Called");
     }
-
+    public void NextButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("NextButton Called");
+    }
+    public void HomeButton()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("HomeButton Called");
+    }
     public void Quit()
     {
         Application.Quit();
